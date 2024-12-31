8abd8e97f26f49fbb5a5588651d47f7e6236fb26
IsmailArrouche <ismailarrouche1@gmail.com>
2024-12-31 10:20:00 +0100

    notif marche

--- src/components/Explore/Nav.js
+++ src/components/Explore/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeMode } from "../../app/features/mode/modeSlice";
 import { SunIcon, MoonIcon } from "@heroicons/react/solid";
-import Contact from "./Contact";
+import NotificationMenu from "./NotificationMenu";
 
 function Navbar() {
     const dispatch = useDispatch();
@@ -65,22 +65,7 @@ function Navbar() {
                     </button>
 
                     {/* Icône de Notifications */}
-                    <button className="hover:text-blue-500 transition-colors">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                            />
-                        </svg>
-                    </button>
+                    <NotificationMenu />
 
                     {/* Icône de changement de mode */}
                     <button
@@ -96,7 +81,7 @@ function Navbar() {
                     </button>
                 </div>
             </nav>
-        </div> 
+        </div>
     );
 }
 
