2bd15cdc427c861e8e27e845b5de58f17201c567
Barchah622 <mpersedal@gmail.com>
2025-01-04 12:03:57 +0100

    branche UserProfile ameliorer

--- src/components/Profile/UserProfile.js
+++ src/components/Profile/UserProfile.js
@@ -1,12 +1,15 @@
 import React from "react";
 import profileImage from "../../../src/assets/avatar.jpeg";
 import coverImage from "../../../src/assets/mountain.jpg";
+import { useSelector } from "react-redux";
 
 const UserProfile = () => {
+    // redux
+    const user = useSelector((state) => state.user.user);
   return (
     <div className="relative bg-[#1B2136] w-full max-w-screen-xl mx-auto rounded-lg shadow-md overflow-hidden sm:w-full lg:w-[60rem]">
      {/* Cover Photo */}
-    <div className="h-48 w-[100%] overflow-hidden mt-4 mx-4">
+    <div className="h-48 lg:w-[100%] overflow-hidden mt-4 mx-4">
       <img
         src={coverImage} // Remplacez par votre URL ou chemin d'image
         alt="Cover"
@@ -25,8 +28,8 @@ const UserProfile = () => {
   </div>
   {/* Informations du Profil */}
   <div className="pl-24"> {/* Ajout de padding-left pour espacer */}
-    <h1 className="text-2xl font-bold">Mohannad Zitoun</h1>
-    <p className="text-sm text-gray-400">support@gmail.com</p>
+    <h1 className="text-2xl font-bold">{user?.firstName} {user?.lastName}</h1>
+    <p className="text-sm text-gray-400">{user?.email}</p>
   </div>
   {/* Boutons d'Action */}
   <div className="flex space-x-2">
@@ -37,20 +40,11 @@ const UserProfile = () => {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-5 w-5"
-        fill="none"
         viewBox="0 0 24 24"
-        stroke="currentColor"
+        fill="currentColor"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M17 8h2a2 2 0 012 2v10a2 2 0 01-2 2H7a2 2 0 01-2-2V10a2 2 0 012-2h2M12 1v8m0 0l-2.5-2.5M12 9l2.5-2.5"
-        />
+        <path d="M3 4a1 1 0 011-1h16a1 1 0 011 1v16a1 1 0 01-1 1H4a1 1 0 01-1-1V4zm2 0v.511L12 12l7-7.489V4H5zm0 2.267V19h14V6.267l-7 7-7-7z" />
       </svg>
-    </button>
-    <button className="bg-white text-gray-700 px-3 py-2 rounded text-sm border">
-      ...
     </button>
   </div>
 </div>
@@ -58,8 +52,8 @@ const UserProfile = () => {
 
       {/* Navigation */}
       <div className="border-t border-gray-700 bg-[#1B2136]">
-        <div className="flex flex-wrap justify-center sm:justify-start space-x-4 px-6 py-3 text-gray-400">
-          {["About", "Membership", "Discussion", "Video", "Group", "Events", "Media"].map(
+        <div className="flex flex-wrap space-x-4 px-5 py-3 text-gray-400">
+          {["About", "Discussion", "Group", "Events", "Media"].map(
             (item) => (
               <a
                 key={item}
