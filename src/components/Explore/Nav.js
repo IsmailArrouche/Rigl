fd184590b44ee2750083d7509c343dce6c942984
yassineahm1 <ahmzaouiyassin@gmail.com>
2025-01-04 17:15:10 +0100

    modification du messagerie

--- src/components/Explore/Nav.js
+++ src/components/Explore/Nav.js
@@ -62,12 +62,26 @@ function Navbar({ toggleSidebar }) {
           </button>
 
           {/* Icône de messagerie */}
-          <button className="hover:text-blue-500 transition-colors relative">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
-</svg>
-
-
+          <button
+            className="hover:text-blue-500 transition-colors relative"
+            onClick={() => {
+              window.location.href = "/message"; // Remplacez par votre lien
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+              />
+            </svg>
             <span className="absolute top-0 right-0 h-2.5 w-2.5 bg-orange-500 rounded-full"></span>
           </button>
 
@@ -89,14 +103,11 @@ function Navbar({ toggleSidebar }) {
 
           {/* Icône de profil (Avatar avec style comme dans l'image) */}
           <div className="relative">
-            <div className="w-8 h-8 bg-pink-500 rounded-full flex items-center justify-center text-white font-bold">
             <img
-                                src="https://randomuser.me/api/portraits/men/1.jpg"
-                                alt={`'Mohannad avatar`}
-                                className="w-8 h-8 rounded-full"
-                                
-                            />
-            </div>
+              src="https://randomuser.me/api/portraits/men/1.jpg"
+              alt="Mohannad avatar"
+              className="w-8 h-8 rounded-full"
+            />
           </div>
         </div>
       </nav>
